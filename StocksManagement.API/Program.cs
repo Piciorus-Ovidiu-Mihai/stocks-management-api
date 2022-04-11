540809676bd8e4c9f0bd45ab4c1276eeb469ca8a
Piciorus Ovidiu-Mihai <60327678+Piciorus-Ovidiu-Mihai@users.noreply.github.com>
2022-04-11 21:49:27 +0300

    use cors

--- StocksManagement.API/Program.cs
+++ StocksManagement.API/Program.cs
@@ -123,7 +123,7 @@ catch (Exception ex)
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration");
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
